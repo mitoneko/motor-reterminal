b9cc6f45952c2f59328093e776662f783d5f6502
mitoneko <facemito@laki.jp>
2022-07-01 20:59:04 +0900

    version 0完成

--- README.md
+++ README.md
@@ -1,34 +1,36 @@
-# Led-PWM-Reterminal
-ReterminalによるGPIOのPWM制御(GPIO18使用)
+# motor-Reterminal
+ReterminalによるGPIOでのモータードライバ
 
-Reterminalにおいて、GPIO18をPWM制御するためのカーネルモジュールのソースです。
+Reterminalにおいて、Hブリッジを利用したモーター制御のためのカーネルモジュールのソースです。
 使用するためには、カーネルモジュールのビルド環境を整えた上で、makeする必要があります。
 dtbsに関しては、次のコマンドで、コンパイルできます。
 
 ```
-$dtc -@ -I dts -O dtb -o extled.dtbo extled.dtso
+$dtc -@ -I dts -O dtb -o motor.dtbo motor.dtso
 ```
 
-コンパイルができれば、生成された、extled.ko extled.dtboを適当なディレクトリに配置し、
+コンパイルができれば、生成された、motor.ko motor.dtboを適当なディレクトリに配置し、
 
 ```
-$ sudo mkdir /sys/kernel/config/device-tree/overlays/extled
-$ sudo cp beep.dtbo /sys/kernel/config/device-tree/overlays/extled/dtbo
+$ sudo mkdir /sys/kernel/config/device-tree/overlays/motor
+$ sudo cp beep.dtbo /sys/kernel/config/device-tree/overlays/motor/dtbo
 ```
 
 で、デバイスツリーを認識させ、
 
 ```
-$sudo insmod extled.ko
+$sudo insmod motor.ko
 ```
 
 で組み込めます。
 
 ```
-$echo 5 | sudo tee /dev/extled0
+$echo 5 | sudo tee /dev/motor0
 ```
 
-で、GPIO18に接続したLEDを点灯できればOKです。5を一桁の適当な数字に変えれば明るさが変わります。
+で、GPIO18・23に接続したモーターを回転できればOKです。5を一桁の適当な数字に変えれば速度が変わります。正の数値で正転、負の数値で逆転となります。
+
+sysfsにて、PWM制御に使用している、周波数及びpwmの粒度を調整できます。/sys/devices/platform/motor@0配下の、period,max\_pwmに設定する値を数値の文字列で書き込んでください。period(周期)の数値の単位はnsで、max\_pwmの数値はpwmで指定する数値の絶対値の最大値となります。
 
 # ソースの2次使用について
 　カーネルモジュールのサンプルとして、ご自由にお使いください。ライセンス条件は、GPLとします。
--- motor.c
+++ motor.c
@@ -17,16 +17,23 @@
 
 static const unsigned int MINOR_BASE = 0; // udev minor番号の始まり
 static const unsigned int MINOR_NUM = 1;  // udev minorの個数
+static const int WIDTH_MAX_INIT = 9;  // pwm widthの最大値
+static const unsigned int PERIOD_INIT = 1000000; // PWM周期初期値
+
 // デバイス全域で使用する変数達
 // motor_probeでメモリ確保する。
 struct motor_device_info {
     unsigned int major; // udev major番号
     struct cdev cdev;
     struct class *class;
-    struct gpio_desc *gpio;
+    struct gpio_desc *forward;
+    struct gpio_desc *backward;
+    struct gpio_desc *cur_out;
     struct hrtimer pwm_timer;
-    unsigned int priod; // pwmベース周期　ns
+    unsigned int period; // pwmベース周期　ns
+    unsigned int width_max; // PWMのwidthの絶対値の最大
     unsigned int on_time; // 周期あたりのLED ON時間 ns
+    int cur_pwm_width;
 };
 
 // /dev/ectled配下のアクセス関数
@@ -59,49 +66,69 @@ static ssize_t motor_read(struct file *fp, char __user *buf, size_t count, loff_
         pr_err("%s:デバイス情報取得失敗\n", __func__);
         return -EBADF;
     }
-    result = put_user(gpiod_get_value(bdev->gpio)+'0', &buf[0]);
+    if (bdev->cur_out == NULL) {
+        result = put_user('0', &buf[0]);
+    } else {
+        result = put_user(gpiod_get_value(bdev->cur_out)+'0', &buf[0]);
+    }
     if (result != 0) return result;
     return 1;
 }
 
 static ssize_t motor_write(struct file *fp, const char __user *buf, size_t count, loff_t *f_pos) {
     struct motor_device_info *bdev = fp->private_data;
-    char outValue;
+    char outValue_s[10];
+    int outValue;
     int result;
-    int pwm_width;
+    unsigned int pwm_width;
 
     if (bdev==NULL) return -EBADF;
     if (count == 0) return 0;
-    result = get_user(outValue, &buf[0]);
-    if (result != 0) return result;
-    
-    hrtimer_cancel(&bdev->pwm_timer);
-    switch (outValue) {
-        case '0':
-            gpiod_set_value(bdev->gpio, 0);
-            pr_devel("%s: writed [%c] \n", __func__, outValue);
-            break;
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-            pwm_width = outValue - '0';
-            bdev->on_time = bdev->priod / 9 * pwm_width;
-            gpiod_set_value(bdev->gpio, 1);
-            hrtimer_start(&bdev->pwm_timer, 
-                    ktime_set(0, bdev->on_time), HRTIMER_MODE_REL);
-            pr_devel("%s: writed [%c] \n", __func__, outValue);
-            break;
-        case '9':
-            gpiod_set_value(bdev->gpio, 1);
-            pr_devel("%s: writed [%c] \n", __func__, outValue);
-            break;
-        default:
-            pr_info("%s: no writed. arg=\"%c\"\n", __func__ , outValue);
+    if (copy_from_user(outValue_s, buf, min(count, (size_t)9)) != 0) {
+        return -EFAULT;
+    }
+    outValue_s[min(count, (size_t)9)] = '\0';
+    pr_devel("%s: recieve string [%s] \n", __func__, outValue_s);
+    if (outValue_s[0] == 'b' || outValue_s[0] == 'B') {
+        hrtimer_cancel(&bdev->pwm_timer);
+        bdev->cur_out = NULL;
+        bdev->cur_pwm_width = 0;
+        gpiod_set_value(bdev->forward, 1);
+        gpiod_set_value(bdev->backward, 1);
+    } else {
+        result = kstrtoint(outValue_s, 10, &outValue);
+        if (result) {
+            pr_devel("%s: other string [%s] \n", __func__, outValue_s);
+            return count;  // 数値以外のものが来たら無視
+        }
+
+        hrtimer_cancel(&bdev->pwm_timer);
+        if (outValue == 0) {
+            bdev->cur_out = NULL;
+            gpiod_set_value(bdev->forward, 0);
+            gpiod_set_value(bdev->backward, 0);
+            bdev->cur_pwm_width = 0;
+        } else {
+            if (bdev->cur_out) { // 前回の出力をリセットする。
+                gpiod_set_value(bdev->cur_out,0);
+            } else {
+                gpiod_set_value(bdev->forward, 0);
+                gpiod_set_value(bdev->backward, 0);
+            }
+            bdev->cur_out = (outValue>0) ? bdev->forward : bdev->backward;
+            pwm_width = (outValue>=0)? outValue : -outValue;
+            pwm_width = min(pwm_width, bdev->width_max);
+            bdev->cur_pwm_width = pwm_width;
+            if (pwm_width == bdev->width_max) {
+                gpiod_set_value(bdev->cur_out, 1);
+            } else {
+                bdev->on_time = bdev->period / bdev->width_max * pwm_width;
+                gpiod_set_value(bdev->cur_out, 1);
+                hrtimer_start(&bdev->pwm_timer, 
+                        ktime_set(0, bdev->on_time), HRTIMER_MODE_REL);
+            }
+        }
+        pr_devel("%s: writed [%d] \n", __func__, outValue);
     }
 
     return count;
@@ -110,21 +137,21 @@ static ssize_t motor_write(struct file *fp, const char __user *buf, size_t count
 // pwm_timer(hrtimer)用の割り込み処理関数
 enum hrtimer_restart pwm_timer_handler(struct hrtimer *timer) {
     struct motor_device_info *bdev = from_timer(bdev, timer, pwm_timer);
-    int next_priod;
+    int next_period;
 
     if (!bdev) {
         pr_err("%s:デバイス情報取得失敗\n", __func__);
         return HRTIMER_NORESTART;
     }
-    int cur_led = gpiod_get_value(bdev->gpio);
+    int cur_led = gpiod_get_value(bdev->cur_out);
     if (cur_led == 1) {
-        next_priod = bdev->priod - bdev->on_time;
-        gpiod_set_value(bdev->gpio, 0);
+        next_period = bdev->period - bdev->on_time;
+        gpiod_set_value(bdev->cur_out, 0);
     } else {
-        next_priod = bdev->on_time;
-        gpiod_set_value(bdev->gpio, 1);
+        next_period = bdev->on_time;
+        gpiod_set_value(bdev->cur_out, 1);
     }
-    hrtimer_forward_now(&bdev->pwm_timer, ktime_set(0, next_priod));
+    hrtimer_forward_now(&bdev->pwm_timer, ktime_set(0, next_period));
     return HRTIMER_RESTART;
 }
 
@@ -171,11 +198,6 @@ static int make_udev(struct motor_device_info *bdev, const char* name) {
         device_create(bdev->class, NULL, MKDEV(bdev->major, minor), NULL, "motor%d", minor);
     }
 
-    // hrtimerの初期化
-    bdev->priod = 1000000;
-    bdev->on_time = 0;
-    hrtimer_init(&bdev->pwm_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL); 
-    bdev->pwm_timer.function = pwm_timer_handler;
     
     return 0;
 
@@ -198,6 +220,82 @@ static void remove_udev(struct motor_device_info *bdev) {
     cdev_del(&bdev->cdev); /* デバイス除去 */
     unregister_chrdev_region(dev, MINOR_NUM); /* メジャー番号除去 */
 }
+
+// sysfs 関係
+// 　period, max_pwm_widthの設定・読み出し
+ssize_t period_show(struct device *dev, struct device_attribute *attr, char *buf) {
+    struct motor_device_info *bdev = dev_get_drvdata(dev);
+    if (!bdev) {
+        pr_alert("%s: Driver data can't get.\n", __func__);
+        return -ENODEV;
+    }
+    return snprintf(buf, PAGE_SIZE, "%u\n", bdev->period);
+}
+
+ssize_t period_store(struct device *dev, struct device_attribute *attr, 
+        const char *buf, size_t count) {
+    struct motor_device_info *bdev = dev_get_drvdata(dev);
+    int result;
+    unsigned int set_value;
+
+    if (!bdev) {
+        pr_alert("%s: Driver data can't get.\n", __func__);
+        return -ENODEV;
+    }
+    
+    result = kstrtouint(buf, 10, &set_value);
+    if (result) {
+        pr_err("%s: 数値以外のものはperiodにできない。[%s])\n", __func__,buf);
+        return -EINVAL;
+    }
+    
+    bdev->period = set_value;
+    return (ssize_t)count;
+}
+
+ssize_t max_pwm_show(struct device *dev, struct device_attribute *attr, char *buf) {
+    struct motor_device_info *bdev = dev_get_drvdata(dev);
+    if (!bdev) {
+        pr_alert("%s: Driver data can't get.\n", __func__);
+        return -ENODEV;
+    }
+    return snprintf(buf, PAGE_SIZE, "%u\n", bdev->width_max);
+}
+
+ssize_t max_pwm_store(struct device *dev, struct device_attribute *attr, 
+        const char *buf, size_t count) {
+    struct motor_device_info *bdev = dev_get_drvdata(dev);
+    int result;
+    unsigned int set_value;
+
+    if (!bdev) {
+        pr_alert("%s: Driver data can't get.\n", __func__);
+        return -ENODEV;
+    }
+    
+    result = kstrtouint(buf, 10, &set_value);
+    if (result) {
+        pr_err("%s: 数値以外のものはperiodにできない。[%s])\n", __func__,buf);
+        return -EINVAL;
+    }
+    
+    bdev->width_max = set_value;
+    return (ssize_t)count;
+}
+
+
+DEVICE_ATTR(period, S_IRUGO | S_IWUSR | S_IWGRP, period_show, period_store);
+DEVICE_ATTR(max_pwm, S_IRUGO | S_IWUSR | S_IWGRP, max_pwm_show, max_pwm_store);
+
+static struct attribute *motor_attrs[] = {
+    &dev_attr_period.attr,
+    &dev_attr_max_pwm.attr,
+    NULL,
+};
+
+static struct attribute_group motor_group = {
+    .attrs = motor_attrs,
+};
 
 // ドライバの初期化　及び　後始末
 static const struct of_device_id of_motor_ids[] = {
@@ -228,12 +326,19 @@ static int motor_probe(struct platform_device *p_dev) {
     dev_set_drvdata(dev, bdev);
 
     // gpioの確保と初期化
-    bdev->gpio = devm_gpiod_get(dev, NULL, GPIOD_OUT_LOW);
-    if (IS_ERR(bdev->gpio)) {
-        result = -PTR_ERR(bdev->gpio);
-        pr_alert("%s: can not get GPIO.ERR(%d)\n", __func__, result);
+    bdev->forward = devm_gpiod_get_index(dev, NULL, 0, GPIOD_OUT_LOW);
+    if (IS_ERR(bdev->forward)) {
+        result = -PTR_ERR(bdev->forward);
+        pr_alert("%s: can not get GPIO.ERR(%d)(forward)\n", __func__, result);
         goto err;
     }
+    bdev->backward = devm_gpiod_get_index(dev, NULL, 1, GPIOD_OUT_LOW);
+    if (IS_ERR(bdev->backward)) {
+        result = -PTR_ERR(bdev->backward);
+        pr_alert("%s: can not get GPIO.ERR(%d)(backward)\n", __func__, result);
+        goto err_backward;
+    }
+    bdev->cur_out = NULL;
 
     // udevの生成
     result = make_udev(bdev, p_dev->name);
@@ -242,11 +347,27 @@ static int motor_probe(struct platform_device *p_dev) {
         goto err_udev;
     }
 
+    // hrtimerの初期化
+    bdev->period = PERIOD_INIT;
+    bdev->width_max = WIDTH_MAX_INIT;
+    bdev->on_time = 0;
+    hrtimer_init(&bdev->pwm_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL); 
+    bdev->pwm_timer.function = pwm_timer_handler;
+    
+    // sysfsの初期化
+    result = sysfs_create_group(&(dev->kobj), &motor_group);
+    if (result != 0) {
+        pr_alert("%s: Can not create sysfs nodes.\n", __func__);
+        goto err_udev;
+    }
+
     pr_info("%s:motor driver init\n",__func__);
     return 0;
 
 err_udev:
-    gpiod_put(bdev->gpio);
+    gpiod_put(bdev->backward);
+err_backward:
+    gpiod_put(bdev->forward);
 err:
     return result;
 }
@@ -255,9 +376,15 @@ static int motor_remove(struct platform_device *p_dev) {
     struct motor_device_info *bdev = dev_get_drvdata(&p_dev->dev);
     remove_udev(bdev);
 
+    // sysfsの開放
+    sysfs_remove_group(&(p_dev->dev.kobj), &motor_group);
+
     // gpioデバイスの開放
-    if (bdev->gpio) {
-        gpiod_put(bdev->gpio);
+    if (bdev->forward) {
+        gpiod_put(bdev->forward);
+    }
+    if (bdev->backward) {
+        gpiod_put(bdev->backward);
     }
 
     pr_info("%s:motor driver unloaded\n",__func__);
@@ -271,7 +398,7 @@ static struct platform_driver motor_driver = {
     .driver = {
         .name = DRIVER_NAME,
         .owner = THIS_MODULE,
-        .of_match_table = of_beep_ids,
+        .of_match_table = of_motor_ids,
     },
 };
 
